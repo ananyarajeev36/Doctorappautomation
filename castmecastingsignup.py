716b4422c74b27ab3310d9e8da29b10a0a7fb553
Ananya <ananya.r@infolitz.com>
2025-03-28 09:59:20 +0530

    after clearing the caches

--- castmecastingsignup.py
+++ castmecastingsignup.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import sync_playwright
 
 # Define login details
-URL = "https://thecastme.com/#/"
-expectedurl="https://thecastme.com/#/talent/view-profile"
+URL = "https://thecastme.com/#/talent/sign-up"
+expectedurl="https://thecastme.com/#/talent/login"
 def test_simple_login():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)  # Set to True to run in headless mode
@@ -11,17 +11,19 @@ def test_simple_login():
         # Navigate to the login page
         page.goto(URL)
         print("Opened the website.")
-        page.wait_for_timeout(5000)
+        page.wait_for_timeout(1000)
         # Click on the login button
-        page.locator("//button[contains(text(), 'Login')]").first.click()
+        page.locator("//button[contains(text(), 'Sign up')]")
         page.wait_for_timeout(1000)
-        page.locator("//input[@placeholder='Email']").fill("athulya@gmail.com")
-        page.wait_for_timeout(1000)
+        page.locator("//input[@placeholder='Email']").fill("vijay1@gmail.com")
+        page.wait_for_timeout(4000)
         page.locator("//input[@placeholder='Password']").fill("Ananya2006@")
+        page.locator("//input[@placeholder='Full Name']").fill("Ananya")
+        # page.locator("//button[contains(text(), 'LOGIN')]").first.click()
         page.wait_for_timeout(5000)
         # Click login button
 
-        page.locator("//button[contains(text(), 'LOGIN')]").first.click()
+        page.locator("//button[contains(text(), 'SUBMIT')]").first.click()
         print("The user should be redirected to the login page.")
         page.wait_for_timeout(5000)  # Adjust timeout if necessary
         
