4b38b6238213e548b82ccd638a8812c3271ea36c
Ananya <ananya.r@infolitz.com>
2025-04-10 11:13:49 +0530

    photo uploading automated

--- castmephotouploading.py
+++ castmephotouploading.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 # Define login details
 URL = "https://thecastme.com/#/talent/login"
 # expectedurl="https://thecastme.com/#/talent/login"
-def test_simple_login():
+def test_photouploading():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)  # Set to True to run in headless mode
         page = browser.new_page()
@@ -28,19 +28,21 @@ def test_simple_login():
         page.get_by_text("Photos").click()
         page.wait_for_timeout(6000)
         page.evaluate("window.scrollBy(0, 8000)")
-        page.locator('img[alt="Upload"]').click()
+        page.wait_for_timeout(6000)
+        # page.locator("xpath=/html/body/div[5]/div/div/div[2]/div/div[2]/label/img").click()
+        # page.locator('img[alt="Upload"]').click()
         print("hai")
-        page.wait_for_timeout(10000)
-        
-        print("hai")
-
-
-
-
-        
-
-        # Close the browser
+        file_path = "C://Users//infolitz//Desktop//1.png"
+        print("Annaya")
+    # Set file on hidden input (bypassing the plus button)
+        page.set_input_files("input[type='file']", file_path)
+        print("Annaya")
+        page.wait_for_timeout(4000)
+        # page.locator("//button[contains(text(), 'Save')]").click()
+        page.get_by_role("button", name="Save").click()
+        print("hey")
+   # Close the browser
         browser.close()
 
 if __name__ == "__main__":
-    test_simple_login()
+    test_photouploading()
